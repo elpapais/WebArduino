28e13689efd0346e9c8dc4bf8bdec531e2cd7b86
residuen <k.bettray@uni-duisburg.de>
2012-10-22 13:08:36 +0200

    * Checking and updating of OUT-Pins and ADC works

--- src/WebServer/script.js
+++ src/WebServer/script.js
@@ -1,5 +1,6 @@
 var ipAdress = "192.168.1.177";
 
+// XMLHttpRequest-Instanz holen
 function xmlHttpObject()
 {
 	var xmlHttp = null;
@@ -24,10 +25,10 @@ function xmlHttpObject()
 	return xmlHttp;
 }
 
-function http(pin, state) {
+// Pinstatus setzen
+function pinState(pin, state) {
 	var xmlHttp = xmlHttpObject();
 	
-	//alert("digitalWrite("+pin+","+state+")");
 	if (xmlHttp)
 	{
 		xmlHttp.open('GET', 'http://'+ipAdress+'/ardcmd:dw:'+pin+':'+state+';', true);
@@ -35,7 +36,6 @@ function http(pin, state) {
 		{
 			if (xmlHttp.readyState == 4) {
 				document.getElementById("ergebnis").innerHTML = "Status von Pin " +pin+" auf " + ((state==1) ? "HIGH" : "LOW") + " setzen";
-				// alert("Antwort:\n"+xmlHttp.responseText);
 			}
 		};
 		
@@ -43,22 +43,37 @@ function http(pin, state) {
 	}
 }
 
+// Pinstatus ueberpruefen und an buttons uebergeben
 function checkPinState()
 {
-				// dout_2 bis dout_9 auslesen
-				for(var i=0; i<=7; i++)
+	var xmlHttp = xmlHttpObject();
+	var data, root, parser, myResult;
+	
+	if (xmlHttp) {
+		xmlHttp.open('GET', 'http://'+ipAdress+'/ardcmd:adc;', true);
+		xmlHttp.onreadystatechange = function ()
+		{
+			if (xmlHttp.readyState == 4) {
+				var xmlResponse = xmlHttp.responseText;
+				
+				xmlDoc = domParser(xmlResponse);
+				// Pins auslesen
+				for(var i=0; i<=7; i++)	// for(var i=0; i<=9; i++)
 				{
 					var node = xmlDoc.getElementsByTagName("data")[i];
-					var value = parseInt(node.firstChild.data); // ("dout_2");
-					var adc_port = i-8;
+					var value = parseInt(node.firstChild.data);
 					
-					//alert ("value is: "+ value);
-					
-					document.getElementById("dout"+adc_port+"_int").innerHTML = value;
-					document.getElementById("adc"+adc_port+"_volt").innerHTML = Math.round(parseFloat(value)*vFactor * 100) / 100;
+					switchElementTextByIdAndState("pin", (i+2), value); // Pin 0 und 1 derzeit für UART reserviert
 				}
+			}
+		};
+		xmlHttp.send(null);
 	}
+	
+	return true;
+}
 
+// Analogeingaenge auslesen
 function analogRead()
 {
 	var xmlHttp = xmlHttpObject();
@@ -71,11 +86,8 @@ function analogRead()
 		{
 			if (xmlHttp.readyState == 4) {
 				var xmlResponse = xmlHttp.responseText;
-				//alert("analogRead:\n"+xmlResponse);
 				
 				xmlDoc = domParser(xmlResponse);
-				//var customerNode = xmlDoc.getElementById('dout_2');
-				//alert("test: "+customerNode);
 								
 				// ADC auslesen
 				for(var i=8; i<=13; i++)
@@ -84,13 +96,9 @@ function analogRead()
 					var value = node.firstChild.data; // ("dout_2");
 					var adc_port = i-8;
 					
-					//alert ("value is: "+ value);
-					
 					document.getElementById("adc"+adc_port+"_int").innerHTML = value;
 					document.getElementById("adc"+adc_port+"_volt").innerHTML = Math.round(parseFloat(value)*vFactor * 100) / 100;
 				}
-				
-				//document.getElementById("adc0_int").innerHTML = "test: "+xmlResponseTest;
 			}
 		};
 		xmlHttp.send(null);
@@ -99,12 +107,12 @@ function analogRead()
 	return true;
 }
 
+// Alle Pins schalten
 function allPins(state) {
 
 	for(var i=2; i<=9; i++)
 	{
-		//alert("allPins "+i);
-		http(i, state);
+		pinState(i, state);
 		
 		if(state == 1)
 		{
@@ -119,6 +127,7 @@ function allPins(state) {
 	}
 }
 
+// Taster-Status vorbereiten
 function switchButton(obj)
 {
 	var object = document.getElementById(obj);
@@ -145,14 +154,15 @@ function switchButton(obj)
 		switchElementTextById(prefix, i);
 		
 		if(document.getElementById(prefix+"_"+i).innerHTML == "ON")
-			http(i, 1);
+			pinState(i, 1);
 		else
-			http(i, 0);
+			pinState(i, 0);
 	}
 	
 	return true;
 }
 
+// Taster-Status setzen
 function switchElementTextById(prefix, sufix)
 {
 	if(document.getElementById(prefix+"_"+sufix).innerHTML == "OFF")
@@ -165,9 +175,31 @@ function switchElementTextById(prefix, sufix)
 		document.getElementById(prefix+"_"+sufix).innerHTML = "OFF";
 		document.getElementById(prefix+"_"+sufix).style.color = "red";
 	}
-
 }
 
+// Taster-Status setzen
+function switchElementTextByIdAndState(prefix, sufix, state)
+{
+	if(state==1)
+	{
+		document.getElementById(prefix+"_"+sufix).innerHTML = "ON";
+		document.getElementById(prefix+"_"+sufix).style.color = "green";
+	}
+	else
+	{
+		document.getElementById(prefix+"_"+sufix).innerHTML = "OFF";
+		document.getElementById(prefix+"_"+sufix).style.color = "red";
+	}
+}
+
+// Pins und ADC aktualisieren
+function update()
+{
+	analogRead();
+	checkPinState();
+}
+
+// DOM-Parser Instanz holen
 function domParser(text)
 {
 	var xmlDoc;
